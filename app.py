5b024f0edf63b3b14def36e54f219a1be1759d02
ga1nang <nnthanhmx@gmail.com>
2024-11-13 21:07:10 +0700

    Fix vector embedding bug and add historical chat

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
 from langchain_community.vectorstores import FAISS
 
 load_dotenv()
@@ -14,12 +14,15 @@ load_dotenv()
 # Load the API keys
 os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
 
-# Initialize sessions in session state if not already done
-def initialize_sessions():
-    if "chat_sessions" not in st.session_state:
-        st.session_state.chat_sessions = {}
-    if "current_session_id" not in st.session_state:
-        st.session_state.current_session_id = None
+
+# Start a new chat session with a welcome message
+def start_new_chat_session():
+    session_id = str(uuid.uuid4())
+    st.session_state.chat_sessions[session_id] = [
+        {"role": "assistant", "content": "Hi, I'm a chatbot who can help you. How can I help you?"}
+    ]
+    st.session_state.current_session_id = session_id
+
 
 # Embedding vector based on chatbot type
 def load_vector_embeddings(chatbot_type):
@@ -40,32 +43,93 @@ def load_vector_embeddings(chatbot_type):
 # Set up the language model and retrieval chain
 def setup_language_model(engine):
     llm = ChatOpenAI(model=engine)
-    prompt = ChatPromptTemplate.from_template(
-        """
-        You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer
-        the question. If you don't know the answer, say that you don't know. Use three sentences maximum and keep the 
-        answer concise.
-        <context>
-        {context}
-        <context>
-        Question: {input}
-        """
+    
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, "
+        "just reformulate it if needed and otherwise return it as is."
+    )
+    
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    
+    system_prompt = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        "{context}"
+    )
+    
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
     )
 
     if "vectors" in st.session_state:
-        document_chain = create_stuff_documents_chain(llm, prompt)
+        #create retriever
         retriever = st.session_state.vectors.as_retriever()
-        st.session_state.retrieval_chain = create_retrieval_chain(retriever, document_chain)
+        
+        #create Q&A chain
+        document_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+        #create history aware chain
+        history_aware_retriever = create_history_aware_retriever(
+            llm, retriever, contextualize_q_prompt
+        )
+        
+        
+        
+        st.session_state.retrieval_chain = create_retrieval_chain(history_aware_retriever, document_chain)
     else:
         st.write("Vectors not initialized. Please check the FAISS index.")
 
-# Start a new chat session with a welcome message
-def start_new_chat_session():
-    session_id = str(uuid.uuid4())
-    st.session_state.chat_sessions[session_id] = [
-        {"role": "assistant", "content": "Hi, I'm a chatbot who can help you. How can I help you?"}
-    ]
-    st.session_state.current_session_id = session_id
+
+
+
+# Initialize sessions in session state if not already done
+def initialize_sessions():
+    if "chat_sessions" not in st.session_state:
+        st.session_state.chat_sessions = {}
+        
+    if "current_session_id" not in st.session_state:
+        st.session_state.current_session_id = None
+        
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+        
+        
+# Set up the sidebar with chatbot options
+def display_sidebar():
+    st.sidebar.header("Chat Sessions")
+
+    if st.sidebar.button("New Chat Session"):
+        start_new_chat_session()
+        st.sidebar.write("New chat session started!")
+
+    session_options = list(st.session_state.chat_sessions.keys())
+    current_session_id = st.sidebar.selectbox("Select a Chat Session", session_options, index=0 if session_options else -1)
+    if current_session_id:
+        st.session_state.current_session_id = current_session_id
+
+    chatbot_type = st.sidebar.selectbox("Select a chatbot type", ['Credit Papa(Credit scoring)', 'Cooking Mama(Cooking master)'])
+    load_vector_embeddings(chatbot_type)
+
+    engine = st.sidebar.selectbox("Select Open AI model", ["gpt-4o", "gpt-3.5-turbo-1106"])
+    setup_language_model(engine)
+
 
 # Display messages for the selected session
 def display_chat_messages():
@@ -73,6 +137,7 @@ def display_chat_messages():
         st.session_state.messages = st.session_state.chat_sessions[st.session_state.current_session_id]
         for msg in st.session_state.messages:
             st.chat_message(msg['role']).write(msg['content'])
+
 
 # Handle new user input and generate a response
 def handle_user_input():
@@ -95,24 +160,6 @@ def handle_user_input():
         else:
             st.error("Retrieval chain not initialized.")
 
-# Set up the sidebar with chatbot options
-def display_sidebar():
-    st.sidebar.header("Chat Sessions")
-
-    if st.sidebar.button("New Chat Session"):
-        start_new_chat_session()
-        st.sidebar.write("New chat session started!")
-
-    session_options = list(st.session_state.chat_sessions.keys())
-    current_session_id = st.sidebar.selectbox("Select a Chat Session", session_options, index=0 if session_options else -1)
-    if current_session_id:
-        st.session_state.current_session_id = current_session_id
-
-    chatbot_type = st.sidebar.selectbox("Select a chatbot type", ['Credit Papa(Credit scoring)', 'Cooking Mama(Cooking master)'])
-    load_vector_embeddings(chatbot_type)
-
-    engine = st.sidebar.selectbox("Select Open AI model", ["gpt-4o", "gpt-4-turbo", "gpt-4"])
-    setup_language_model(engine)
 
 # Main application logic
 def main():
--- src/vector_embedding.py
+++ src/vector_embedding.py
@@ -1,82 +1,75 @@
 import streamlit as st
 import os
-
-from langchain_groq import ChatGroq
+from langchain_community.vectorstores import FAISS
+from tempfile import TemporaryDirectory
+from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
-from langchain_community.vectorstores import FAISS
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from tempfile import TemporaryDirectory
-from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 
 os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
 embeddings = OpenAIEmbeddings()
 
-
-#set up streamlit
 st.title('Update the vector store')
 st.write('Upload your pdf')
 
+# Set up the temp directory
 temp_dir = "RAG_Q&A_Conversation"
 os.makedirs(temp_dir, exist_ok=True)
 
+# Track chatbot_type with session state
+if 'previous_chatbot_type' not in st.session_state:
+    st.session_state.previous_chatbot_type = None
 
-#process uploaded PDF
+# Select chatbot type and track any change
+chatbot_type = st.sidebar.selectbox('Select a chatbot type to load data', ['credit', 'cooking'])
+if chatbot_type != st.session_state.previous_chatbot_type:
+    st.session_state.previous_chatbot_type = chatbot_type
+    st.session_state.vector_store_loaded = False  # Flag to reload the vector store
+
+# Process uploaded PDF
 uploaded_files = st.file_uploader("Choose a PDF file", type='pdf', accept_multiple_files=True)
 start = st.button('Generate Embedding')
+
 if start:
     with st.spinner('Generating Embedding....'):
-        #load data
+        # Load data
         documents = []
         with TemporaryDirectory() as temp_dir:
             for uploaded_file in uploaded_files:
                 temp_pdf = os.path.join(temp_dir, f"temp_{uploaded_file.name}")
                 with open(temp_pdf, 'wb') as file:
                     file.write(uploaded_file.getvalue())
-                    file_name = uploaded_file.name
                 
-                #load the pdf file and save to documents    
+                # Load the PDF file and save to documents
                 loader = PyPDFLoader(temp_pdf)
                 docs = loader.load()
                 documents.extend(docs)
-                    
-            
-                
-            #split and create embeddings for the documents
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=500)
-            splits = text_splitter.split_documents(documents)
-            
-            
-            
-            # Load previous vector store
-            vector_store_path = 'store'
-            try:
-                #add new data to vector store
-                vector_store = FAISS.load_local(vector_store_path, embeddings=embeddings, allow_dangerous_deserialization=True)
-                vector_store.add_documents(splits, embeddings=embeddings)
-                
-            except (FileNotFoundError, RuntimeError) as e:
-                #create new vector store
-                if "could not open" in str(e) or "No such file or directory" in str(e):
-                    st.warning("No existing vector store found. A new vector store will be created.")
-                    vector_store = FAISS.from_documents(documents=splits, embedding=embeddings)
-                else:
-                    raise e
-            
-            
 
-            # Save the updated vector store
-            vector_store.save_local(vector_store_path)
-     
+        # Split and create embeddings for the documents
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=500)
+        splits = text_splitter.split_documents(documents)
+
+        # Load or create the vector store
+        vector_store_path = 'store/' + chatbot_type
+        try:
+            if not st.session_state.vector_store_loaded:
+                vector_store = FAISS.load_local(vector_store_path, embeddings=embeddings, allow_dangerous_deserialization=True)
+                st.session_state.vector_store_loaded = True  # Flag as loaded
+            vector_store.add_documents(splits, embeddings=embeddings)
+
+        except (FileNotFoundError, RuntimeError) as e:
+            if "could not open" in str(e) or "No such file or directory" in str(e):
+                st.warning("No existing vector store found. A new vector store will be created.")
+                vector_store = FAISS.from_documents(documents=splits, embedding=embeddings)
+            else:
+                raise e
+
+        # Save the updated vector store
+        vector_store.save_local(vector_store_path)
+        st.success("Vector store updated and saved successfully.")
 
 st.success("Vector store updated and saved successfully.")
 
